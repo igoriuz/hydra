2f1cd9d20d2c2e161dd7deea3d55aa5f8887959a
Igor Kazhdan <igork3010@gmail.com>
2026-02-18 20:56:36 +0100

    chore: remove unused HydraNeck and commented-out context_behavior_ext

--- lib/hydra.dart
+++ lib/hydra.dart
@@ -1,8 +1,7 @@
-/// Flutter widget which helps building responsive widgets
+/// Flutter widget which helps building responsive widgets.
 library hydra;
 
 export 'package:hydra/src/hydra_behaviour.dart';
 export 'package:hydra/src/hydra_head.dart';
 export 'package:hydra/src/hydra_widget.dart';
 export 'package:hydra/src/hydra_no_widget_exception.dart';
-export 'package:hydra/src/context_behavior_ext.dart';
