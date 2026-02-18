7c4bd1544621eb0d687219ccfe2df6b47fc0bea8
Igor Kazhdan <igork3010@gmail.com>
2026-02-18 21:50:08 +0100

    refactor: remove redundant breakpoint.dart imports from tests

--- test/hydra_head_test.dart
+++ test/hydra_head_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hydra/hydra.dart';
-import 'package:hydra/src/breakpoint.dart';
 
 void main() {
   group('HydraHead factories', () {
