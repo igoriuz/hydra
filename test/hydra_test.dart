f7281407a7998d4fcff7e6dcb94b24db67c20165
Igor Kazhdan <igork3010@gmail.com>
2026-02-18 21:09:22 +0100

    test: update tests for null safety and modern assertions

--- test/hydra_test.dart
+++ test/hydra_test.dart
@@ -4,288 +4,222 @@ import 'package:hydra/hydra.dart';
 import 'package:hydra/src/breakpoint.dart';
 
 void main() {
-  group('testing hydraWidget', () {
-    Widget mini, small, medium, large;
-    HydraBehaviour multiScreenBehaviour;
-    var screenBP = 0.0;
-
-    setUpAll(() {
-      mini = Container();
-      small = Container();
-      medium = Container();
-      large = Container();
-    });
+  group('HydraWidget', () {
+    final mini = Container();
+    final small = Container();
+    final medium = Container();
+    final large = Container();
 
     group('with only one element in list', () {
-      test(
-          'returns mini if only [mini] is available and meets the mini breakpoint range',
-          () {
-        screenBP = 15;
-        var hydra = HydraWidget(
-          mini: mini,
-        );
+      test('returns mini when only mini is available at mini breakpoint', () {
+        const screenBP = 15.0;
+        final hydra = HydraWidget(mini: mini);
 
-        assert(hydra.widgets.length == 1);
+        expect(hydra.widgets.length, 1);
         expect(hydra.nearestWidget(screenBP).breakpoint, Breakpoint.mini);
       });
 
-      test(
-          'returns small if only [small] is available while breakpoint is large',
+      test('returns small when only small is available at large breakpoint',
           () {
-        screenBP = kLargeBP;
-        var hydra = HydraWidget(
-          small: small,
-        );
+        const screenBP = kLargeBP;
+        final hydra = HydraWidget(small: small);
 
-        assert(hydra.widgets.length == 1);
+        expect(hydra.widgets.length, 1);
         expect(hydra.nearestWidget(screenBP).breakpoint, Breakpoint.small);
       });
 
-      test(
-          'returns medium if only [medium] is available while breakpoint is small',
+      test('returns medium when only medium is available at small breakpoint',
           () {
-        screenBP = kSmallBP;
-        var hydra = HydraWidget(
-          medium: medium,
-        );
+        const screenBP = kSmallBP;
+        final hydra = HydraWidget(medium: medium);
 
-        assert(hydra.widgets.length == 1);
+        expect(hydra.widgets.length, 1);
         expect(hydra.nearestWidget(screenBP).breakpoint, Breakpoint.medium);
       });
 
-      test(
-          'returns large if only [large] is available while breakpoint is large',
+      test('returns large when only large is available at large breakpoint',
           () {
-        screenBP = kLargeBP;
-        var hydra = HydraWidget(
-          large: large,
-        );
+        const screenBP = kLargeBP;
+        final hydra = HydraWidget(large: large);
 
-        assert(hydra.widgets.length == 1);
+        expect(hydra.widgets.length, 1);
         expect(hydra.nearestWidget(screenBP).breakpoint, Breakpoint.large);
       });
     });
 
     group('next bigger screen', () {
-      test('small if only [mini, small] is available with breakpoint large',
-          () {
-        screenBP = kLargeBP;
-        var hydra = HydraWidget(
-          mini: mini,
-          small: small,
-        );
+      test('small if only [mini, small] at large breakpoint', () {
+        const screenBP = kLargeBP;
+        final hydra = HydraWidget(mini: mini, small: small);
 
-        assert(hydra.widgets.length == 2);
+        expect(hydra.widgets.length, 2);
         expect(hydra.nearestWidget(screenBP).breakpoint, Breakpoint.small);
       });
 
-      test('large if only [mini, large] is available with breakpoint medium',
-          () {
-        screenBP = kMediumBP;
-        var hydra = HydraWidget(
-          mini: mini,
-          large: large,
-        );
+      test('large if only [mini, large] at medium breakpoint', () {
+        const screenBP = kMediumBP;
+        final hydra = HydraWidget(mini: mini, large: large);
 
-        assert(hydra.widgets.length == 2);
+        expect(hydra.widgets.length, 2);
+        expect(hydra.nearestWidget(screenBP).breakpoint, Breakpoint.large);
+      });
+
+      test('large if only [mini, small, large] at medium breakpoint', () {
+        const screenBP = kMediumBP;
+        final hydra = HydraWidget(mini: mini, small: small, large: large);
+
+        expect(hydra.widgets.length, 3);
         expect(hydra.nearestWidget(screenBP).breakpoint, Breakpoint.large);
       });
 
       test(
-          'large if only [mini, small, large] is available with breakpoint medium',
+          'small if only [mini, small, large] at width between small and medium',
           () {
-        screenBP = kMediumBP;
-        var hydra = HydraWidget(
-          mini: mini,
-          small: small,
-          large: large,
-        );
+        const screenBP = kSmallBP + 5;
+        final hydra = HydraWidget(mini: mini, small: small, large: large);
 
-        assert(hydra.widgets.length == 3);
-        expect(hydra.nearestWidget(screenBP).breakpoint, Breakpoint.large);
-      });
-
-      test(
-          'small if only [mini, small, large] is available with width between small and medium',
-          () {
-        screenBP = kSmallBP + 5; // range between small and medium
-        var hydra = HydraWidget(
-          mini: mini,
-          small: small,
-          large: large,
-        );
-
-        assert(hydra.widgets.length == 3);
+        expect(hydra.widgets.length, 3);
         expect(hydra.nearestWidget(screenBP).breakpoint, Breakpoint.small);
       });
     });
 
-    group('test preferred screen attribute', () {
-      Size verticalSize;
-      Size horizontalSize;
-
-      setUpAll(() {
-        verticalSize = Size(300, 700);
-        horizontalSize = Size(700, 300);
-      });
-
-      test(
-          'shortest width if orientation awarness is not relevant with vertical device',
+    group('orientation awareness', () {
+      test('uses shortestSide when orientation awareness is off (vertical)',
           () {
-        multiScreenBehaviour = HydraBehaviour.noOrientation();
-        var hydra = HydraWidget(
+        const verticalSize = Size(300, 700);
+        final hydra = HydraWidget(
           mini: mini,
-          behaviour: multiScreenBehaviour,
+          behaviour: const HydraBehaviour.noOrientation(),
         );
         expect(hydra.comparableWidth(verticalSize), verticalSize.shortestSide);
       });
 
-      test(
-          'shortest width if orientation awarness is not relevant with horizontal device',
+      test('uses shortestSide when orientation awareness is off (horizontal)',
           () {
-        multiScreenBehaviour = HydraBehaviour.noOrientation();
-        var hydra = HydraWidget(
+        const horizontalSize = Size(700, 300);
+        final hydra = HydraWidget(
           mini: mini,
-          behaviour: multiScreenBehaviour,
+          behaviour: const HydraBehaviour.noOrientation(),
         );
         expect(
             hydra.comparableWidth(horizontalSize), horizontalSize.shortestSide);
       });
 
-      test('current width is shortestSide if orientation awarness is relevant',
-          () {
-        multiScreenBehaviour = HydraBehaviour(isOrientationAware: true);
-        var hydra = HydraWidget(
+      test('uses shortestSide for vertical device when orientation aware', () {
+        const verticalSize = Size(300, 700);
+        final hydra = HydraWidget(
           mini: mini,
-          behaviour: multiScreenBehaviour,
+          behaviour: const HydraBehaviour(isOrientationAware: true),
         );
         expect(hydra.comparableWidth(verticalSize), verticalSize.shortestSide);
       });
 
-      test('current width is longestSide if orientation awarness is relevant',
+      test(
+          'uses width (longestSide) for horizontal device when orientation aware',
           () {
-        multiScreenBehaviour = HydraBehaviour(isOrientationAware: true);
-        var hydra = HydraWidget(
+        const horizontalSize = Size(700, 300);
+        final hydra = HydraWidget(
           mini: mini,
-          behaviour: multiScreenBehaviour,
+          behaviour: const HydraBehaviour(isOrientationAware: true),
         );
         expect(
             hydra.comparableWidth(horizontalSize), horizontalSize.longestSide);
       });
     });
 
-    group('next smaller screen', () {
-      setUpAll(() {
-        multiScreenBehaviour = HydraBehaviour.preferSmaller();
-      });
-
-      test('small if only [mini, small] is available', () {
-        screenBP = kLargeBP;
-        var hydra = HydraWidget(
+    group('prefer smaller screen', () {
+      test('small if only [mini, small] at large breakpoint', () {
+        const screenBP = kLargeBP;
+        final hydra = HydraWidget(
           mini: mini,
           small: small,
-          behaviour: multiScreenBehaviour,
+          behaviour: const HydraBehaviour.preferSmaller(),
         );
 
         expect(hydra.nearestWidget(screenBP).breakpoint, Breakpoint.small);
       });
 
-      test('large if only [mini, large] is available', () {
-        multiScreenBehaviour = HydraBehaviour(isSmallerScreenPreferred: false);
-        screenBP = kMediumBP;
-        var hydra = HydraWidget(
+      test(
+          'large if only [mini, large] at medium breakpoint (bigger preferred)',
+          () {
+        const screenBP = kMediumBP;
+        final hydra = HydraWidget(
           mini: mini,
           large: large,
-          behaviour: multiScreenBehaviour,
+          behaviour: const HydraBehaviour(isSmallerScreenPreferred: false),
         );
 
         expect(hydra.nearestWidget(screenBP).breakpoint, Breakpoint.large);
       });
     });
 
-    group('same screen size', () {
-      setUpAll(() {
-        multiScreenBehaviour = HydraBehaviour.preferSmaller();
-      });
-
-      test('small if only [mini, small] is available', () {
-        screenBP = 0;
-        var hydra = HydraWidget(
+    group('exact breakpoint match', () {
+      test('mini at width 0', () {
+        const screenBP = 0.0;
+        final hydra = HydraWidget(
           mini: mini,
           small: small,
-          behaviour: multiScreenBehaviour,
+          behaviour: const HydraBehaviour.preferSmaller(),
         );
 
         expect(hydra.nearestWidget(screenBP).breakpoint, Breakpoint.mini);
       });
 
-      test('medium if [mini, medium, large] is available', () {
-        screenBP = kMediumBP;
-        var hydra = HydraWidget(
+      test('medium at medium breakpoint', () {
+        const screenBP = kMediumBP;
+        final hydra = HydraWidget(
           mini: mini,
           medium: medium,
           large: large,
-          behaviour: multiScreenBehaviour,
+          behaviour: const HydraBehaviour.preferSmaller(),
         );
 
         expect(hydra.nearestWidget(screenBP).breakpoint, Breakpoint.medium);
       });
 
-      test(
-          'small if only [small, mini, large] with medium size and smaller screen preferred',
-          () {
-        screenBP = kMediumBP;
-        multiScreenBehaviour = HydraBehaviour.preferSmaller();
-        var hydra = HydraWidget(
+      test('small at medium width with smaller preferred', () {
+        const screenBP = kMediumBP;
+        final hydra = HydraWidget(
           small: small,
           mini: mini,
           large: large,
-          behaviour: multiScreenBehaviour,
+          behaviour: const HydraBehaviour.preferSmaller(),
         );
 
         expect(hydra.nearestWidget(screenBP).breakpoint, Breakpoint.small);
       });
 
-      test(
-          'large if only [small, mini, large] with medium size and bigger screen preferred',
-          () {
-        multiScreenBehaviour = HydraBehaviour(isSmallerScreenPreferred: false);
-        screenBP = kMediumBP;
-        var hydra = HydraWidget(
+      test('large at medium width with bigger preferred', () {
+        const screenBP = kMediumBP;
+        final hydra = HydraWidget(
           small: small,
           mini: mini,
           large: large,
-          behaviour: multiScreenBehaviour,
+          behaviour: const HydraBehaviour(isSmallerScreenPreferred: false),
         );
 
         expect(hydra.nearestWidget(screenBP).breakpoint, Breakpoint.large);
       });
 
-      test(
-          'large if only [small, mini, large] with medium size and bigger screen preferred',
-          () {
-        multiScreenBehaviour = HydraBehaviour(isSmallerScreenPreferred: false);
-        screenBP = 0;
-        var hydra = HydraWidget(
+      test('mini at width 0 with bigger preferred', () {
+        const screenBP = 0.0;
+        final hydra = HydraWidget(
           small: small,
           mini: mini,
           large: large,
-          behaviour: multiScreenBehaviour,
+          behaviour: const HydraBehaviour(isSmallerScreenPreferred: false),
         );
 
         expect(hydra.nearestWidget(screenBP).breakpoint, Breakpoint.mini);
       });
 
-      test(
-          'large if only [small, mini, large] with medium size and bigger screen preferred',
-          () {
-        multiScreenBehaviour = HydraBehaviour(isSmallerScreenPreferred: true);
-        screenBP = kLargeBP;
-        var hydra = HydraWidget(
+      test('medium at large width with smaller preferred', () {
+        const screenBP = kLargeBP;
+        final hydra = HydraWidget(
           small: small,
           mini: mini,
           medium: medium,
-          behaviour: multiScreenBehaviour,
+          behaviour: const HydraBehaviour(isSmallerScreenPreferred: true),
         );
 
         expect(hydra.nearestWidget(screenBP).breakpoint, Breakpoint.medium);
