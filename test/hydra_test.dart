304780fbdf1d116cd678cdcfccf47faac07752dc
Igor Kazhdan <igork3010@gmail.com>
2026-02-18 21:46:58 +0100

    test: add tests for Material Design preset

--- test/hydra_test.dart
+++ test/hydra_test.dart
@@ -225,5 +225,25 @@ void main() {
         expect(hydra.nearestWidget(screenBP).breakpoint, Breakpoint.medium);
       });
     });
+
+    group('material preset', () {
+      test('uses Material Design breakpoints', () {
+        final hydra = HydraWidget(
+          mini: mini,
+          small: small,
+          medium: medium,
+          large: large,
+          behaviour: const HydraBehaviour.material(),
+        );
+
+        expect(hydra.nearestWidget(0).breakpoint, Breakpoint.mini);
+        expect(hydra.nearestWidget(599).breakpoint, Breakpoint.mini);
+        expect(hydra.nearestWidget(600).breakpoint, Breakpoint.small);
+        expect(hydra.nearestWidget(839).breakpoint, Breakpoint.small);
+        expect(hydra.nearestWidget(840).breakpoint, Breakpoint.medium);
+        expect(hydra.nearestWidget(1199).breakpoint, Breakpoint.medium);
+        expect(hydra.nearestWidget(1200).breakpoint, Breakpoint.large);
+      });
+    });
   });
 }
