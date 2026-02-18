3254ca2f3c592d5a46781fbff054d7f4d270d592
Igor Kazhdan <igork3010@gmail.com>
2026-02-18 21:00:26 +0100

    refactor: modernize HydraWidget with null safety, switch expressions, bug fix

--- lib/src/hydra_widget.dart
+++ lib/src/hydra_widget.dart
@@ -1,102 +1,92 @@
 import 'package:flutter/material.dart';
-import 'hydra_head.dart';
-import 'hydra_behaviour.dart';
+
 import 'breakpoint.dart';
+import 'hydra_behaviour.dart';
+import 'hydra_head.dart';
 import 'hydra_no_widget_exception.dart';
 
-/// [HydraWidget] is a [StatelessWidget] that decides based on the [behaviour] which
-/// widget from a list of [widgets] will be shown for the current screen type.
+/// [HydraWidget] is a [StatelessWidget] that selects which widget to display
+/// based on the current screen size and [behaviour] configuration.
 ///
-/// [behaviour] can be controlled by creating a [HydraBehaviour] object.
-///
-/// Four screen alternatives are currently supported.
+/// Up to four screen alternatives are supported: [mini], [small], [medium],
+/// and [large]. At least one must be provided.
 class HydraWidget extends StatelessWidget {
-  /// {@macro hydrabehaviour}
+  /// {@macro hydra_behaviour}
   final HydraBehaviour behaviour;
 
-  final List<HydraHead> widgets = [];
+  /// The resolved list of widget alternatives, ordered by preference.
+  final List<HydraHead> widgets;
 
   HydraWidget({
-    Key key,
+    super.key,
     this.behaviour = const HydraBehaviour(),
-    Widget mini,
-    Widget small,
-    Widget medium,
-    Widget large,
-  }) : super(key: key) {
-    var heads = <HydraHead>[];
+    Widget? mini,
+    Widget? small,
+    Widget? medium,
+    Widget? large,
+  }) : widgets = _buildWidgetList(
+         mini: mini,
+         small: small,
+         medium: medium,
+         large: large,
+         preferSmaller: behaviour.isSmallerScreenPreferred,
+       );
 
-    if (large != null) {
-      heads.add(HydraHead.large(large));
-    }
-    if (medium != null) {
-      heads.add(HydraHead.medium(medium));
-    }
-    if (small != null) {
-      heads.add(HydraHead.small(small));
-    }
-    if (mini != null) {
-      heads.add(HydraHead.mini(mini));
-    }
+  static List<HydraHead> _buildWidgetList({
+    required Widget? mini,
+    required Widget? small,
+    required Widget? medium,
+    required Widget? large,
+    required bool preferSmaller,
+  }) {
+    final heads = <HydraHead>[
+      if (large != null) HydraHead.large(large),
+      if (medium != null) HydraHead.medium(medium),
+      if (small != null) HydraHead.small(small),
+      if (mini != null) HydraHead.mini(mini),
+    ];
 
     if (heads.isEmpty) {
       throw HydraNoWidgetException('At least one widget is needed');
     }
 
-    widgets.addAll(behaviour.isSmallerScreenPreferred ? heads.reversed : heads);
+    return preferSmaller ? heads.reversed.toList() : heads;
   }
 
   @override
   Widget build(BuildContext context) {
     final width = comparableWidth(MediaQuery.of(context).size);
-
     return nearestWidget(width).widget;
   }
 
+  /// Returns the comparable width based on orientation awareness.
   double comparableWidth(Size size) {
     return behaviour.isOrientationAware ? size.width : size.shortestSide;
   }
 
+  /// Finds the best matching [HydraHead] for the given [comparable] width.
   HydraHead nearestWidget(double comparable) {
-    var nearestBreakpoint = Breakpoint.large;
-
-    if (comparable < behaviour.breakpointLarge) {
-      nearestBreakpoint = Breakpoint.medium;
-    }
-    if (comparable < behaviour.breakpointMedium) {
-      nearestBreakpoint = Breakpoint.small;
-    }
-    if (comparable < behaviour.breakpointSmall) {
-      nearestBreakpoint = Breakpoint.mini;
-    }
+    final effectiveBreakpoint = _breakpointForWidth(comparable);
 
     return widgets.firstWhere(
-      (element) => (element.breakpoint.index == nearestBreakpoint.index),
-      orElse: () => chooseAvailableWidget(nearestBreakpoint),
+      (element) => element.breakpoint == effectiveBreakpoint,
+      orElse: () => _closestAlternative(effectiveBreakpoint),
     );
   }
 
-  HydraHead chooseAvailableWidget(Breakpoint nearestBP) {
-    HydraHead nextPossible;
-    var distance = Breakpoint.values.length;
+  Breakpoint _breakpointForWidth(double width) {
+    if (width < behaviour.breakpointSmall) return Breakpoint.mini;
+    if (width < behaviour.breakpointMedium) return Breakpoint.small;
+    if (width < behaviour.breakpointLarge) return Breakpoint.medium;
+    return Breakpoint.large;
+  }
 
-    // collect possible alternatives
-    var alternatives = <HydraHead>[
-      ...widgets.where((element) =>
-          (element.breakpoint.index <= nearestBP.index) ||
-          (element.breakpoint.index >= nearestBP.index))
-    ];
-
-    // choose the widget with shortest distance to nearest breakpoint
-    for (var hydraHead in alternatives) {
-      var current = (nearestBP.index - hydraHead.breakpoint.index).abs();
-
-      if (current < distance) {
-        nextPossible = hydraHead;
-        distance = current;
-      }
-    }
-
-    return nextPossible;
+  /// Finds the widget with the shortest distance to [target] breakpoint.
+  HydraHead _closestAlternative(Breakpoint target) {
+    return widgets.reduce((a, b) {
+      final distA = (target.index - a.breakpoint.index).abs();
+      final distB = (target.index - b.breakpoint.index).abs();
+      return distA <= distB ? a : b;
+    });
   }
 }
